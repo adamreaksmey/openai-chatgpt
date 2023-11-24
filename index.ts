d98f0c35427da672cd9756cb30a3485615c849d3
adamreaksmey <adaminiature@gmail.com>
2023-11-24 18:56:52 +0700

    added ts

--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ import Sitemapper from "sitemapper";
 import OpenAI from "openai";
 
 const openai = new OpenAI();
-console.log(openai)
+console.log('hello world')
+
+const fullWebsiteAssistant = async () => {
+
+}
