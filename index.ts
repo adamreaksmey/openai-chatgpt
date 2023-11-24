0096ba7ce6405e6c5cbbfc18a251b70a16e8a14d
adamreaksmey <adaminiature@gmail.com>
2023-11-24 19:59:47 +0700

    update

--- index.ts
+++ index.ts
@@ -5,34 +5,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const apiKey = process.env.OPEN_AI_KEY;
-const openai = new OpenAI({
-  apiKey: apiKey,
-});
 
-// console.log("hello world");
-// console.log(process.env.OPEN_AI_KEY);
+const openai = new OpenAI({ apiKey: apiKey });
 
-const fullWebsiteAssistant = async (url: string): Promise<Assistant> => {
-  const sitemap = new Sitemapper({ url });
-  const { sites } = await sitemap.fetch();
-
-  const files: any = await Promise.all(
-    sites.map(async (site) => {
-      const fileResult = await openai.files.create({
-        file: await fetch(site),
-        purpose: "assistants",
-      });
-
-      return fileResult;
-    })
-  );
-
-  const openaiResult = await openai.beta.assistants.create({
-    instructions: "You are adam's assistant.",
-    model: "gpt-4-1106-preview",
-    tools: [{ type: "retrieval" }],
-    file_ids: files.map((file: any) => file.id),
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
   });
 
-  return openaiResult;
-};
+  console.log(completion.choices[0]);
+}
+
+main();
