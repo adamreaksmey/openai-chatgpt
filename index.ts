41e9442de9903ab8d56c958fae81d42c4c3dd513
adamreaksmey <adaminiature@gmail.com>
2023-11-25 14:19:32 +0700

    updated pr

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
-import {askRLineQuestion } from "./functions/ask"
+import { askRLineQuestion } from "./functions/ask"
+import { quizJson } from "./quiz/index"
 
 dotenv.config();
 const apiKey = process.env.OPEN_AI_KEY;
