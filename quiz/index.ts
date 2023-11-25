ed1955423de338886de8161ad4bf1a278c557bfd
adamreaksmey <adaminiature@gmail.com>
2023-11-25 14:19:32 +0700

    updated pr

---
+++ quiz/index.ts
@@ -0,0 +1,31 @@
+const quizJson: object = {
+  name: "display_quiz",
+  description:
+    "Displays a quiz to the student, and returns the student's response. A single quiz can have multiple questions.",
+  parameters: {
+    type: "object",
+    properties: {
+      title: { type: "string" },
+      questions: {
+        type: "array",
+        description:
+          "An array of questions, each with a title and potentially options (if multiple choice).",
+        items: {
+          type: "object",
+          properties: {
+            question_text: { type: "string" },
+            question_type: {
+              type: "string",
+              enum: ["MULTIPLE_CHOICE", "FREE_RESPONSE"],
+            },
+            choices: { type: "array", items: { type: "string" } },
+          },
+          required: ["question_text"],
+        },
+      },
+    },
+    required: ["title", "questions"],
+  },
+};
+
+export { quizJson };
