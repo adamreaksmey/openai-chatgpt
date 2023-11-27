fb135730c491f261860f5056e0cc96a96d840107
adamreaksmey <adaminiature@gmail.com>
2023-11-27 11:58:06 +0700

    added displayQuiz function, fixed 2d array type

---
+++ functions/displayQuiz.ts
@@ -0,0 +1,30 @@
+const displayQuiz = async (
+  title: string,
+  questions: Record<string, string>[],
+  askRLineQuestion: any
+) => {
+  console.log("Quiz:\n", title);
+  let responses: string[][] = [];
+
+  for (const question of questions) {
+    let response: string[] = [];
+
+    // if multiple choice, print options
+    if (question["question_type"] === "MULTIPLE_CHOICE") {
+      const rLineQn = `Question: ${question["question_text"]}\n
+        Options: ${question["choices"]}\n
+        `;
+
+      response = await askRLineQuestion(rLineQn);
+    } else if (question["question_type"] === "FREE_RESPONSE") {
+      const rLineQn = `Question: ${question["question_text"]}\n
+        `;
+
+      response = await askRLineQuestion(rLineQn);
+    }
+
+    responses.push(response);
+  }
+  console.log("Your responses from the quiz:\n", responses);
+  return responses;
+};
--- index.ts
+++ index.ts
@@ -26,9 +26,57 @@ const main = async () => {
     });
 
     console.log("Log on no errors.");
-    const thread = await openai.beta.threads.create();
 
-    console.log("First log");
+    // threads created
+    const thread = await openai.beta.threads.create();
+    // Use continueConversation as state for keep asking questions
+    let continueConversation = true;
+    let isQuizAnswered = false;
+
+    while (continueConversation) {
+      const userQuestion = isQuizAnswered
+        ? await askRLineQuestion("You next question to the model: \n")
+        : // this will make the model  build a quiz using our provided function
+          "Make a quiz with 2 questions: One open ended, one multiple choice" +
+          "Then, give me feedback for the responses.";
+
+      // Pass in the user question into the existing thread
+      await openai.beta.threads.messages.create(thread.id, {
+        role: "user",
+        content: userQuestion,
+      });
+    }
+
+    // Use runs to wait for the assistant response and then retrieve it
+    const run = await openai.beta.threads.runs.create(thread.id, {
+      assistant_id: assistant.id,
+    });
+
+    let actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+
+    // Polling mechanism to see if actualRun is completed
+    while (
+      actualRun.status === "queued" ||
+      actualRun.status === "in_progress" ||
+      actualRun.status === "requires_action"
+    ) {
+      if (actualRun.status === "requires_action") {
+        // extra single tool call
+        const toolCall =
+          actualRun.required_action?.submit_tool_outputs?.tool_calls[0];
+
+        const name = toolCall?.function.name;
+
+        const args = JSON.parse(toolCall?.function?.arguments || "{}");
+        const questions = args.questions;
+
+        const responses = await displayQuiz(name || "cool quiz", questions);
+
+        // toggle flag that sets initial quiz
+        isQuizAnswered = true;
+        
+      }
+    }
   } catch (error) {
     console.log(error);
   }
