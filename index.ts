df2059a0851d4dcd208e6cfe857f557aa5e83a69
adamreaksmey <adaminiature@gmail.com>
2023-11-27 13:09:16 +0700

    created status run function

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { askRLineQuestion } from "./functions/ask";
 import { quizJson } from "./quiz/index";
 import { displayQuiz } from "./functions/displayQuiz";
+import { statusRun } from "./functions/statusRun";
+
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -60,44 +62,16 @@ const main = async () => {
     let actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
 
     // Polling mechanism to see if actualRun is completed
-    while (
-      actualRun.status === "queued" ||
-      actualRun.status === "in_progress" ||
-      actualRun.status === "requires_action"
-    ) {
-      if (actualRun.status === "requires_action") {
-        // extra single tool call
-        const toolCall =
-          actualRun.required_action?.submit_tool_outputs?.tool_calls[0];
-
-        const name = toolCall?.function.name;
-
-        const args = JSON.parse(toolCall?.function?.arguments || "{}");
-        const questions = args.questions;
-
-        const responses = await displayQuiz(
-          name || "cool quiz",
-          questions,
-          askRLineQuestion
-        );
-
-        // toggle flag that sets initial quiz
-        isQuizAnswered = true;
-
-        // we must submit the tool outputs to the run to continue
-        await openai.beta.threads.runs.submitToolOutputs(thread.id, run.id, {
-          tool_outputs: [
-            {
-              tool_call_id: toolCall?.id,
-              output: JSON.stringify(responses),
-            },
-          ],
-        });
-      }
-      // keep polling until the run is completed
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-    }
+    const params = {
+      actualRun,
+      askRLineQuestion,
+      isQuizAnswered,
+      openai,
+      thread,
+      run,
+      displayQuiz,
+    };
+    await statusRun(params);
 
     // Get the last assistant message from the messages array
     const messages = await openai.beta.threads.messages.list(thread.id);
